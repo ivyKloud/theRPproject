82113657a346c1cf880b2626176cc3b61795bdba
ivyKloud <ivykloud@gmail.com>
2024-03-04 02:42:41 +0400

    display cards dynamically

--- packages/main/src/components/CardList/CardList.tsx
+++ packages/main/src/components/CardList/CardList.tsx
@@ -1,9 +1,25 @@
 import { Card, GridContainer } from "@repo/ui"
 
+const data = [
+    {
+        title:'title1',
+        subtitle:'subtitle1',
+        href:'https://google.fr',
+        color: '#E3561A',
+    },
+    {
+        title:'title2',
+        subtitle:'subtitle2',
+        href:'https://google.fr',
+        color: '#468864',
+    }
+]
 export const CardList = ({}) => {
     return (
         <GridContainer>
-            <Card title='title' subtitle="subtitle" href="https://google.fr" color="#112233" />
+            {data.map(({title, subtitle, href, color}) => 
+                <Card title={title} subtitle={subtitle} href={href} color={color} />
+            )}
         </GridContainer>
     )
 }
