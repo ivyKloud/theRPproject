1d048520efceed4e491e5f2c18e734517021674e
ivyKloud <ivykloud@gmail.com>
2024-03-04 00:32:00 +0400

    add new package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,23 +1,23 @@
-import { Card } from "@repo/ui/card";
+import { Card } from "../../../packages/ui/src/Card";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
+import { TheProjectApp } from "@repo/main";
 
 export default function Page(): JSX.Element {
   return (
     <main className={styles.main}>
-
       <Button appName="web" className={styles.button}>
         Click me!
       </Button>
+      <TheProjectApp />
 
-
-      <div className={styles.grid}>
+      {/* <div className={styles.grid}>
         {[].map(({ title, href, description }) => (
           <Card className={styles.card} href={href} key={title} title={title}>
             {description}
           </Card>
         ))}
-      </div>
+      </div> */}
     </main>
   );
 }
---
+++ packages/main/src/app/App.tsx
@@ -0,0 +1,4 @@
+
+export const TheProjectApp = () => {
+    return <>Hello Project App</>
+}
